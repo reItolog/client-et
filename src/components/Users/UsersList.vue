220c425549426fbd07957aee96d089c261bfad70
reitolog <repasslo1T>
2020-10-07 14:54:19 +0300

    remove actions buttons in UsersList

--- src/components/Users/UsersList.vue
+++ src/components/Users/UsersList.vue
@@ -10,33 +10,6 @@
           <v-list-item-title v-html="item.displayName"></v-list-item-title>
           <v-list-item-subtitle v-html="item.email"></v-list-item-subtitle>
         </v-list-item-content>
-
-        <v-btn
-          class="mx-2"
-          fab
-          dark
-          small
-          color="primary"
-          :data-id="item.uid"
-          @click="handleDetail($event)"
-        >
-          <v-icon dark>
-            mdi-eye
-          </v-icon>
-        </v-btn>
-        <v-btn
-          class="mx-2"
-          fab
-          dark
-          small
-          color="primary"
-          :data-id="item.uid"
-          @click="handleDelete($event)"
-        >
-          <v-icon dark>
-            mdi-delete
-          </v-icon>
-        </v-btn>
       </v-list-item>
 
       <v-divider :key="index" :inset="item.inset"></v-divider>
@@ -45,19 +18,9 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
 export default {
   props: ['users'],
-  methods: {
-    ...mapActions(['getUserById']),
-    handleDelete(event) {
-      const id = event.currentTarget.dataset.id;
-      console.log(id);
-    },
-  },
-  created() {},
 };
 </script>
 
-<style>
-</style>
+<style></style>
